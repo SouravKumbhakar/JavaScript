da2ed239aafc22ae77072a6530c79854c516ffd4
unix0055 <souravkumbhakar90@gmail.com>
2024-04-21 00:59:10 +0530

    add new initial

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,1 +1,24 @@
- 
+ const course ={
+    coursename: "js in indi",
+    price:"999",
+    courseInstractor:"hitesh"
+ }
+
+//  course.courseInstractor
+
+// const{courseInstractor} = course
+ const{courseInstractor : instractor} = course
+
+
+// console.log(courseInstractor) //hitesh
+console.log(instractor); //hitesh
+
+
+//API>> jabbhi apko apna kaam kisi aur ke upar dalna 
+//hota hai usiko api kahte hai
+
+// {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// } 
--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-console.log(jsUser.age);
-// console.log(jsUser["age"]); //same output
-
-// console.log(jsUser["Full name"]);// in this dot method not applied
-// console.log(jsUser[mySym]);
+price
